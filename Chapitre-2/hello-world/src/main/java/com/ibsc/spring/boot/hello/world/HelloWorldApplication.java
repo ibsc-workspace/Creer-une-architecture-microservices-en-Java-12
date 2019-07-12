4c188a781afc5553a37f7682fa5764ee3ff1879e
Mohamed IBN EL AZZOUZI <mohamed.ibn-elazzouzi@ibsc-world.com>
2019-07-12 02:20:03 +0100

    Initialiser le projet hello-world

---
+++ Chapitre-2/hello-world/src/main/java/com/ibsc/spring/boot/hello/world/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.ibsc.spring.boot.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(HelloWorldApplication.class, args);
+  }
+
+}
